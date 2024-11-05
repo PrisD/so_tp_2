4b9f6dd34d7f3c9e2ea25c22cccb11d94efb4486
prisd <prisciladellavecchia@gmail.com>
2024-11-05 15:05:03 -0300

    punto 1 completo

--- punto-1/chatfifo.c
+++ punto-1/chatfifo.c
@@ -8,42 +8,53 @@ tipea la palabra “bye” el proceso deja de enviar y recibir mensajes a travé
 los fifo’s. */
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  int fd[2];
+  int fd_escritura, fd_lectura;
   /* dependiendo los argumentos lee o escribe el proceso */
   char *fifo_escritor = argv[1];
   char *fifo_lector = argv[2];
   char buffer[1000];
 
+  fd_escritura = open(fifo_escritor, O_RDWR);
+  fd_lectura = open(fifo_lector, O_RDONLY);
+
   printf("Chat iniciado. Escriba 'bye' para salir.\n");
-  while (1) {
-    if (read(fd[0], buffer, 100) > 0) {
-      printf("Recibido: %s\n", buffer);
-      if (strncmp(buffer, "bye", 3) == 0) {
-        printf("El otro usuario ha terminado el chat.\n");
+
+  pid_t pid = fork();
+
+  if (pid == 0) {
+    while (1) {
+      memset(buffer, 0, sizeof(buffer));
+      if (read(fd_lectura, buffer, sizeof(buffer))) {
+        if (!strcmp(buffer, "bye\n")) {
+          printf("El chat ha finalizado\n");
+          break;
+        } else {
+          printf("%s", buffer);
+        }
+      }
+    }
+  } else {
+    while (1) {
+      fgets(buffer, sizeof(buffer), stdin);
+      write(fd_escritura, buffer, sizeof(buffer));
+      if (!strcmp(buffer, "bye\n")) {
+        printf("Has finalizado el chat\n");
         break;
       }
     }
-
-    printf("Tú: ");
-    fgets(buffer, 100, stdin);
-    buffer[strcspn(buffer, "\n")] = '\0';
-
-    write(fd[1], buffer, strlen(buffer) + 1);
-
-    if (strcmp(buffer, "bye") == 0) {
-      printf("Terminando el chat.\n");
-      break;
-    }
+    wait(0);
   }
 
-  close(fd[1]);
-  close(fd[0]);
+  close(fd_escritura);
+  close(fd_lectura);
 
   return 0;
 }
